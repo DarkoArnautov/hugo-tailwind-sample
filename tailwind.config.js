e263144add71edfeac46380a724852dd569a1902
DarkoArnautov <darkoservantes@outlook.com>
2024-12-08 17:20:08 +0100

    feat:add initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./layouts/**/*.html", "./content/**/*.md", "./themes/**/*.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        dmsans: ['DM Sans', 'sans-serif'],
+        inter: ['Inter', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+};
